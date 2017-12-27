1e58b5576986295e07ce67b39eadee5a15da619c
Albert <albert@techlifeguru.net>
2017-12-27 16:38:10 -0600

    Implement MatchGame.generateCardValues() function

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,4 +1,11 @@
-var MatchGame = {};
+getRandomInt = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+};
+
+var MatchGame = {
+  numbers:[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8],
 
 /*
   Sets up a new game after HTML document has loaded.
@@ -9,8 +16,31 @@ var MatchGame = {};
   Generates and returns an array of matching card values.
  */
 
-MatchGame.generateCardValues = function () {
+generateCardValues : function () {
+  var randomNumbers = [];
+  var i = 0;
 
+  while(MatchGame.numbers.length !== 0){
+
+    // Generate random index between 0 and 15
+    var randomIndex = getRandomInt(0, MatchGame.numbers.length); 
+
+    // Insert a random number from numbers array 
+    // in the randomized array
+    randomNumbers.splice(i, 0, MatchGame.numbers[randomIndex]);
+
+    // Remove number from the numbers array
+    MatchGame.numbers.splice(randomIndex, 1);
+
+    i++;
+  }
+
+  // Reinitialize numbers array
+  MatchGame.numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+
+  return(randomNumbers);
+},
+ 
 };
 
 /*
